18247e28e301eb95c14e6b4c6a43e1ef6cb5489b
Krystle <krebates@gmail.com>
2014-02-13 18:41:02 -0500

    updated the file to have the tests passing with stubbing with the VCR gem. api key is labeled sensitive

--- spec/movie/movie_spec.rb
+++ spec/movie/movie_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 require 'pry'
 
-describe OnConnect::Movie do
+
+describe OnConnect::Movie, :vcr do
 	it 'allows me to search for a local theater and list movies' do	
 		movies = OnConnect::Movie.search('10009', '2014-02-14', 1)
 		expect(movies).to_not eql([])

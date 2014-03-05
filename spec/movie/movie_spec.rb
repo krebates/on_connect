458d8772228da9c95c8c74ba63ad8e0899fa1c75
Krystle <krebates@gmail.com>
2014-03-05 14:03:24 -0500

    updated test to have start date to have the current date. test passes

--- spec/movie/movie_spec.rb
+++ spec/movie/movie_spec.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 describe OnConnect::Movie, :vcr do
 	it 'allows me to search for a local theater and list movies' do	
-		movies = OnConnect::Movie.search_zip('10009', '2014-02-20', 1)
+		movies = OnConnect::Movie.search_zip('10009', Time.now.strftime('%Y-%m-%d'), 1)
 		expect(movies).to_not eql([])
 		expect(movies.first).to be_kind_of(OnConnect::Movie)
 
